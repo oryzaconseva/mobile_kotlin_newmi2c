de68bddcf61bb45be51a8306fd070e7da889c572
oryzaconseva <161263449+oryzaconseva@users.noreply.github.com>
2024-08-29 20:48:12 +0700

    latihan 2 dan 4

---
+++ pemograman mobile/app/src/main/java/com/oryzac/myapplication/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.oryzac.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val txtNama = findViewById<EditText>(R.id.txtNama)
+        val txtPass = findViewById<EditText>(R.id.txtPass)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener(){
+            val user = txtNama.text.toString()
+            val pass = txtPass.text.toString()
+
+            if(user == "admin" && pass == "123456"){
+                val intent = Intent(this, welcome::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Username atau Password anda salah", Toast.LENGTH_SHORT).show()
+            }
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
